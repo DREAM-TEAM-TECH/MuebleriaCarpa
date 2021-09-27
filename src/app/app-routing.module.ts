227a2479eebd42e6336959216cfa60c50a5d4604
Juanda1050 <danielalejandre1050@gmail.com>
2021-09-27 13:25:51 -0500

    Cambiar titulo e imagen de aplicacion web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,17 +12,16 @@ const routes: Routes = [
     loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
   },
   {
-<<<<<<< HEAD
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-=======
+  },
+  {
     path: 'new-product',
     loadChildren: () => import('./new-product/new-product.module').then( m => m.NewProductPageModule)
   },
   {
     path: 'display-products',
     loadChildren: () => import('./display-products/display-products.module').then( m => m.DisplayProductsPageModule)
->>>>>>> product
   }
 
 ];
