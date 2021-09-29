b29563846f7ea2a9554f77f94b5d36bcf76b77f5
GranAdrianX <pajaroazulx@gmail.com>
2021-09-29 18:51:05 -0500

    nosepa2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,15 +4,6 @@
 
 export const environment = {
   production: false,
-  firebaseConfig:{
-    apiKey: "AIzaSyBn6ap6hawz90NVNx-8X8NRtF9COXIxnBQ",
-    authDomain: "muebleria-928f9.firebaseapp.com",
-    projectId: "muebleria-928f9",
-    storageBucket: "muebleria-928f9.appspot.com",
-    messagingSenderId: "1035603308487",
-    appId: "1:1035603308487:web:5bf5e6ee20e481e349f3e2",
-    measurementId: "G-DVDZL4CCCD"
-  }
 };
 
 export const firebaseConfig = {
